93989e5872c4acad314c1140cac0cf7f151876fc
vtch99 <crystal.tyan@gmail.com>
2015-01-09 22:44:18 +0800

    created

---
+++ MidasSpider/settings.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for MidasSpider project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'MidasSpider'
+
+SPIDER_MODULES = ['MidasSpider.spiders']
+NEWSPIDER_MODULE = 'MidasSpider.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'MidasSpider (+http://www.yourdomain.com)'
